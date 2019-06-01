a7318f341cb02bc50315ff0c981a6b04d3393673
Edward <galikeev.eduard@yandex.ru>
2019-06-01 08:04:51 +0500

    Это наш первый коммит

---
+++ script.js
@@ -0,0 +1,40 @@
+'use strict';
+
+// var number = 5;
+// var string = "Hello";
+// var sym = Symbol();
+// var boolean = true;
+// null;
+// undefined;
+// var obj = {};
+
+
+// console.log(4/0);
+// console.log(boolean);
+
+// let something;
+// console.log(something);
+
+// let persone = {
+//   name: "John",
+//   age: 25,
+//   isMarried: false
+// };
+
+// console.log(persone.name);
+
+// let n = 100;
+// let s = n + " бутылок пива на стене.";
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+console.log(arr[1]);
+
+// console.log(s);
+
+// alert("Hello world!");
+
+// let answer = confirm("Ты здесь?");
+// console.log(answer);
+
+let answer = prompt("Ты здесь?", "Yes");
+console.log(typeof(arr));
