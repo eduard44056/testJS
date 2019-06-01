ea18764b1855570a132eb2c0db38e15a8b8f763e
Edward <galikeev.eduard@yandex.ru>
2019-06-01 18:13:59 +0500

    Доьавление файла js.js

---
+++ js.js
@@ -0,0 +1,11 @@
+let num = 50;
+
+if (num < 49) {
+  console.log('Неверно!')
+}else if (num > 100) {
+  console.log('Много!')
+}else {
+    console.log('Верно')
+};
+
+(num == 50) ? console.log('Верно!') : console.log('Неверно!');
